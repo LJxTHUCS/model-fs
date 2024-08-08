c668cf3658cdf9a2e9ce9a4edc3da62ca1e3fbd9
liujingx <liujingx21@mails.tsinghua.edu.cn>
2024-08-08 15:20:16 +0800

    feat: use string based path type

--- src/path.rs
+++ src/path.rs
@@ -6,17 +6,11 @@ use std::{
 };
 
 /// Absolute file path.
-#[derive(Clone, PartialEq, Eq, Hash)]
-pub struct AbsPath(Vec<String>);
-
-impl Debug for AbsPath {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "/{}", self.0.join("/"))
-    }
-}
+#[derive(Debug, Clone, PartialEq, Eq, Hash, AbstractState)]
+pub struct AbsPath(String);
 
 impl Deref for AbsPath {
-    type Target = Vec<String>;
+    type Target = String;
     fn deref(&self) -> &Self::Target {
         &self.0
     }
@@ -28,42 +22,21 @@ impl DerefMut for AbsPath {
     }
 }
 
-impl AbstractState for AbsPath {
-    fn matches(&self, other: &Self) -> bool {
-        self.0 == other.0
-    }
-    fn update(&mut self, other: &Self) {
-        self.0 = other.0.clone();
-    }
-}
-
 impl From<Path> for AbsPath {
     /// Convert a `km_command::fs::Path` to an `AbsPath`.
     ///
     /// Callers should ensure that the `Path` is absolute.
     fn from(path: Path) -> Self {
-        Self(
-            path.strip_prefix("/")
-                .unwrap()
-                .split("/")
-                .map(|s| s.to_owned())
-                .collect(),
-        )
+        Self(path.to_string())
     }
 }
 
 /// Relative file path.
-#[derive(Clone, PartialEq, Eq, Hash)]
-pub struct RelPath(Vec<String>);
-
-impl Debug for RelPath {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0.join("/"))
-    }
-}
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct RelPath(String);
 
 impl Deref for RelPath {
-    type Target = Vec<String>;
+    type Target = String;
     fn deref(&self) -> &Self::Target {
         &self.0
     }
@@ -80,38 +53,37 @@ impl From<Path> for RelPath {
     ///
     /// Callers should ensure that the `Path` is relative.
     fn from(path: Path) -> Self {
-        Self(path.split("/").map(|s| s.to_owned()).collect())
+        Self(path.to_string())
     }
 }
 
 impl AbsPath {
     /// Create a new absolute path.
-    pub fn new(path: Vec<String>) -> Self {
+    pub fn new(path: String) -> Self {
         Self(path)
     }
 
     /// Root absolute path.
     pub fn root() -> Self {
-        Self(vec![])
+        Self("/".to_owned())
     }
 
     /// Concatenate a relative path to this absolute path.
     pub fn join(&self, rel_path: &RelPath) -> Self {
-        Self(
-            self.0
-                .clone()
-                .into_iter()
-                .chain(rel_path.0.clone())
-                .collect(),
-        )
+        let mut path = self.0.clone();
+        path.push('/');
+        path.push_str(&rel_path.0);
+        Self(path)
     }
 
     /// Get the parent directory of this absolute path.
     pub fn parent(&self) -> Option<Self> {
-        if self.0.is_empty() {
+        if self.0 == "/" {
             None
         } else {
-            Some(Self(self[..self.len() - 1].to_vec()))
+            let mut components = self.split('/').collect::<Vec<_>>();
+            components.pop();
+            Some(Self(components.join("/")))
         }
     }
 }
@@ -119,10 +91,11 @@ impl AbsPath {
 impl RelPath {
     /// Current directory.
     pub fn cur() -> Self {
-        Self(vec![".".to_owned()])
+        Self(".".to_owned())
     }
+
     /// Parent directory.
     pub fn parent() -> Self {
-        Self(vec!["..".to_owned()])
+        Self("..".to_owned())
     }
 }
